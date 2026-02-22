42d2632eea6a63e93bb9c82a629d85db99fc8663
Ayush Khemka <ayushkhemkaka@gmail.com>
2026-02-22 19:43:25 +0530

    Fix

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,16 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth.config";
 
-const nextAuth = NextAuth as unknown as (config: Record<string, unknown>) => {
+type NextAuthResult = {
   handlers: unknown;
-  auth: unknown;
-  signIn: unknown;
-  signOut: unknown;
+  auth: (...args: any[]) => Promise<any>;
+  signIn: (...args: any[]) => Promise<any>;
+  signOut: (...args: any[]) => Promise<any>;
 };
+
+const nextAuth = NextAuth as unknown as (
+  config: Record<string, unknown>
+) => NextAuthResult;
 
 export const { handlers, auth, signIn, signOut } = nextAuth({
   ...authConfig,
